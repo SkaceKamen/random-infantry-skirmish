7ccaf84f0d93ccb9e778c75b3cdbfe6792c6942e
Jan Zípek <jan@zipek.cz>
2017-05-01 17:28:27 +0200

    Better waiting dialog

--- dialogs/waitingDialog.hpp
+++ dialogs/waitingDialog.hpp
@@ -14,11 +14,16 @@ class RSTF_RscDialogWaiting
 		class title : RscStatic
 		{
 			x = 0.5-0.2;
-			y = 0;
+			y = 0.1;
 			w = 0.4;
 			h = 0.1;
-			colorBackground[] = { "(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",0.7 };
-			text = "Waiting for host";
+			colorBackground[] = {
+				"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+				"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+				"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+				0.8
+			};
+			text = "Waiting on host";
 			style = ST_CENTER;
 		};
 	};
