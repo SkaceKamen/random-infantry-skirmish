fd2dd1823e8d870ff4e18c132390931b01707dfc
Jan Zípek <jan@zipek.cz>
2017-05-03 17:22:11 +0200

    Moved declarations out of hpp file, so it can be included in sqf

--- dialogs/advancedConfigDialog.hpp
+++ dialogs/advancedConfigDialog.hpp
@@ -1,26 +1,12 @@
-#define RSTF_ADV_X (SafeZoneX + 0.2)
-#define RSTF_ADV_Y (SafeZoneY + 0.2)
-#define RSTF_ADV_W (SafeZoneW - 0.4)
-#define RSTF_ADV_H (SafeZoneH - 0.4)
+#include "advancedConfigDialog.inc"
 
-#define RSTF_ADV_M 0.01
-
-#define RSTF_ADV_TITLE_H 0.04
-
-#define RSTF_ADV_CNT_Y (RSTF_ADV_Y + RSTF_ADV_TITLE_H + RSTF_ADV_M)
-#define RSTF_ADV_CNT_H (RSTF_ADV_H - RSTF_ADV_TITLE_H - RSTF_ADV_M)
-
-#define RSTF_ADV_CAT_W (RSTF_ADV_W * 0.2)
-
-#define RSTF_ADV_OPS_X (RSTF_ADV_X + RSTF_ADV_CAT_W)
-#define RSTF_ADV_OPS_W (RSTF_ADV_W * 0.8)
-
-#define RSTF_ADV_BUTTON_W 0.1
-#define RSTF_ADV_BUTTON_H 0.05
-#define RSTF_ADV_BUTTON_Y (RSTF_ADV_Y + RSTF_ADV_H + RSTF_ADV_M)
-
-#define RSTF_ADV_SAVE_X (RSTF_ADV_X + RSTF_ADV_W - RSTF_ADV_BUTTON_W)
-#define RSTF_ADV_REST_X (RSTF_ADV_X + RSTF_ADV_W - RSTF_ADV_BUTTON_W * 2 - RSTF_ADV_M)
+class RSTF_ADV_CATEGORY: RscButton
+{
+	x = 0;
+	y = 0;
+	w = RSTF_ADV_CAT_W;
+	h = 0.1;
+};
 
 class RSTF_RscDialogAdvancedConfig
 {
@@ -29,7 +15,14 @@ class RSTF_RscDialogAdvancedConfig
     movingEnable = false;
     class controlsBackground
     {
-		
+		class optionsContainer: RscStatic
+		{
+			x = RSTF_ADV_OPS_X;
+			y = RSTF_ADV_CNT_Y;
+			w = RSTF_ADV_OPS_W;
+			h = RSTF_ADV_CNT_H;
+			colorBackground[] = { 0, 0, 0, 0.8 };
+		};
 	};
 	class controls
 	{
