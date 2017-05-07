3e84eb7056f276e0d14aac37d094e56aa83d588b
Jan Zípek <jan@zipek.cz>
2017-05-07 09:03:33 +0200

    Added new map selection dialog.

---
+++ dialogs/battleSelectionDialog.hpp
@@ -0,0 +1,95 @@
+#define RSTF_BTL_W 0.3
+#define RSTF_BTL_H 0.2
+#define RSTF_BTL_X (SafeZoneX + SafeZoneW/2 - RSTF_BTL_W)
+#define RSTF_BTL_Y (SafeZoneY + SafeZoneH - RSTF_BTL_H)
+
+#define RSTF_BTL_P 0.02
+#define RSTF_BTL_M 0.01
+
+#define RSTF_BTL_TITLE_H 0.05
+
+#define RSTF_BTL_BTS_W RSTF_BTL_W
+#define RSTF_BTL_BTS_H 0.05
+
+#define RSTF_BTL_LIST_X (RSTF_BTL_X)
+#define RSTF_BTL_LIST_Y (RSTF_BTL_Y + RSTF_BTL_TITLE_H + RSTF_BTL_M)
+#define RSTF_BTL_LIST_W (RSTF_BTL_W)
+#define RSTF_BTL_LIST_H (RSTF_BTL_H - RSTF_BTL_TITLE_H - RSTF_BTL_BTS_H - RSTF_BTL_M * 2)
+
+#define RSTF_BTL_BTS_X (RSTF_BTL_X)
+#define RSTF_BTL_BTS_Y (RSTF_BTL_LIST_Y + RSTF_BTL_LIST_H + RSTF_BTL_M)
+
+#define RSTF_BTL_VIEWBUTTON_W (RSTF_BTL_BTS_W * 0.4)
+#define RSTF_BTL_3DBUTTON_X (RSTF_BTL_BTS_X)
+#define RSTF_BTL_2DBUTTON_X (RSTF_BTL_BTS_X + RSTF_BTL_VIEWBUTTON_W + RSTF_BTL_M)
+#define RSTF_BTL_VOTEBUTTON_X (RSTF_BTL_BTS_X + RSTF_BTL_BTS_W - RSTF_BTL_VIEWBUTTON_W)
+
+class RSTF_RscDialogBattleSelection
+{
+    idd = 15;
+    enableDisplay = true;
+    movingEnable = false;
+    class controlsBackground
+    {
+		class battlesContainer: RscStatic
+		{
+			x = RSTF_BTL_LIST_X;
+			y = RSTF_BTL_LIST_Y;
+			w = RSTF_BTL_LIST_W;
+			h = RSTF_BTL_LIST_H;
+			colorBackground[] = { 0, 0, 0, 0.8 };
+		};
+	};
+	class controls
+	{
+		class mainTitle: RscStatic
+		{
+			idc = 1;
+			x = RSTF_BTL_X;
+			y = RSTF_BTL_Y;
+			w = RSTF_BTL_W;
+			h = RSTF_BTL_H;
+			colorBackground[] = { TITLE_BG_RGBA };
+			text = "Vote for map";
+		};
+
+		class battlesContainer: RscListNBox
+		{
+			idc = 2;
+			x = RSTF_BTL_LIST_X + RSTF_BTL_P;
+			y = RSTF_BTL_LIST_Y + RSTF_BTL_P;
+			w = RSTF_BTL_LIST_W - RSTF_BTL_P*2;
+			h = RSTF_BTL_LIST_H - RSTF_BTL_P*2;
+		};
+
+		class button3D: RscButton
+		{
+			idc = 3;
+			x = RSTF_BTL_3DBUTTON_X;
+			y = RSTF_BTL_BTS_Y;
+			w = RSTF_BTL_VIEWBUTTON_W;
+			h = RSTF_BTL_BTS_H;
+			text = "3D";
+		};
+
+		class button2D: RscButton
+		{
+			idc = 4;
+			x = RSTF_BTL_2DBUTTON_X;
+			y = RSTF_BTL_BTS_Y;
+			w = RSTF_BTL_VIEWBUTTON_W;
+			h = RSTF_BTL_BTS_H;
+			text = "2D";
+		};
+
+		class buttonVote: RscButton
+		{
+			idc = 5;
+			x = RSTF_BTL_VOTEBUTTON_X;
+			y = RSTF_BTL_BTS_Y;
+			w = RSTF_BTL_VIEWBUTTON_W;
+			h = RSTF_BTL_BTS_H;
+			text = "Vote";
+		};
+	};
+};
