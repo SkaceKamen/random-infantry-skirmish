13de9de045cc84e50e6252a272e9ab82799bb007
Jan Zípek <jan@zipek.cz>
2018-01-28 11:32:09 +0100

    Added money display and made messages a bit smaller

--- dialogs/titles.hpp
+++ dialogs/titles.hpp
@@ -9,16 +9,19 @@ class rscTitles
         enableSimulation = true;
         enableDisplay = true;
         onLoad = "uinamespace setVariable ['ARCADE_UI', _this select 0]";
-		
+
 		class controls
 		{
 			class GameScore : RscStructuredText
 			{
 				idc = 1;
-				x = SafeZoneX + SafeZoneW / 2 - 0.15;
+				x = SafeZoneX + SafeZoneW / 2 - 0.25;
 				y = SafeZoneY + SafeZoneH - 0.3;
-				w = 0.3;
+				w = 0.5;
 				h = 0.2;
+
+				size = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 0.8)";
+
 				class Attributes
 				{
 					font = "PuristaMedium";
@@ -37,9 +40,8 @@ class rscTitles
 				w = 0.10;
 				h = 0.05;
 				colorBackground[] = { 0, 0.77, 0, 0.9 };
-
 			};
-			
+
 			class ENScoreBar : RscStatic
 			{
 				idc = 2;
@@ -48,6 +50,20 @@ class rscTitles
 				w = 0.10;
 				h = 0.05;
 				colorBackground[] = { 0.9, 0.14, 0.14, 0.9 };
+			};
+
+			class MoneyBar : RscStatic
+			{
+				idc = 5;
+				style = ST_CENTER | ST_VCENTER;
+				shadow = false;
+				colorBackground[] = { 0, 0, 0, 0.8 };
+				color[] = { 1, 1, 1, 1 };
+
+				x = SafeZoneX + SafeZoneW - 0.2;
+				w = 0.2;
+				y = SafeZoneY + SafeZoneH - 0.05;
+				h = 0.05;
 			};
 		};
 	};
